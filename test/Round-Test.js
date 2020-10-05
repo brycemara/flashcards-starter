dd21b110bc3afc15ee7b41cc54cc033f51584129
Bryce Mara Jarrett <brycej98@gmail.com>
2020-10-05 17:26:21 -0500

    Add Round class

---
+++ test/Round-Test.js
@@ -0,0 +1,15 @@
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+});
